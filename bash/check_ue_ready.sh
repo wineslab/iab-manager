769bc8fb2955d8f0cd79bfbc70e544e6b23c1d88
Eugenio Moro <eugenio.moro@polimi.it>
2022-05-24 19:14:26 -0400

    bash script to wait until ue is ready

---
+++ bash/check_ue_ready.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# first check if softmodem is running
+if pgrep 'softmodem' > /dev/null; then
+        echo "Softmodem running..."
+else
+        echo "Softmodem not running, exiting..."; exit 1
+fi
+
+stop_loop=false
+ln_check="============================================"
+max_trials=21
+trials=0
+while [ "$stop_loop" = false ]
+do
+##      echo "trial $trials"
+        let "trials++"
+        if [ $max_trials -eq $trials ]; then
+                echo "Max trial reached, exiting..."
+                exit 1
+        fi
+        ln=$(tail -2 last_log | head -1 | rev | cut -d ' ' -f1)
+        if [ "$ln" = "$ln_check" ]; then
+                echo "Softmodem ready"
+                stop_loop=true # for future flexibility
+                exit 0
+        else
+                echo "Softmodem not ready, waiting 1 second...";
+                sleep 1
+        fi
+done
