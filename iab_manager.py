a5978559dbcace1d8901e60bad7fecad299613d7
Gabriele Gemmi <gabriele.gemmi@unive.it>
2022-11-10 01:37:15 -0500

    Automatic retrival of reservation

--- iab_manager.py
+++ iab_manager.py
@@ -1,81 +1,27 @@
-# This is an iab network manager
-
-
 import networkx as nx
 import configargparse as argparse
-from dotenv import load_dotenv
 from pathlib import Path
 from python.SRN import Srn, SrnTypes
 from python.IabNet import IabNet
 from python.CmdPrompt import PromptWorker
 from fabric import Connection
-from typing import List
-import os
-import requests
-requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
-
-load_dotenv()
-
-COLOSSEUM_USER = os.getenv('COLOSSEUM_USER')
-COLOSSEUM_PWD = os.getenv('COLOSSEUM_PWD')
-
-
-def login_colosseum():
-    json_data = {
-        'username': COLOSSEUM_USER,
-        'password': COLOSSEUM_PWD,
-    }
-    response = requests.post('https://experiments.colosseum.net/api/v1/auth/login/',  json=json_data,  verify=False)
-    if response.status_code != 200:
-        raise Exception("Error logging in")
-    for c in response.cookies:
-        if c.name == 'sessionid':
-            return c.value
-    raise Exception("No cookie found")
-
-
-def get_reservation(session_id, reservation_id):
-    cookies = {
-        'sessionid': session_id,
-    }
-
-    headers = {
-        'Accept': 'application/json, text/plain, */*',
-    }
-
-    response = requests.get(f'https://experiments.colosseum.net/api/v1/reservations/{reservation_id}/', cookies=cookies, headers=headers, verify=False)
-    if response.status_code != 200:
-        raise Exception("Error getting reservation")
-    return response.json()
-
-
-def parse_snr_from_reservation(reservation_id: str, blacklist: list[int]) -> List[Srn]:
-    session = login_colosseum()
-    data = get_reservation(session, reservation_id)[0]
-    snr_list = []
-    for node in data['nodes']:
-        nid = node['srn_id']
-        if nid not in blacklist:
-            hn = data['team_name'] + '-' + ('00' if nid <= 9 else '0' if nid <= 99 else '') + str(nid)
-            snr_list.append(Srn(hostname=hn, id=node['srn_id']))
-    snr_list.sort(key=lambda x: x.id)
-    return snr_list
+from python.Colosseum import Colosseum
 
 
 def manager_init(args, sounding):
     topology: nx.DiGraph = nx.read_graphml(args.topology)
-
+    colosseum = Colosseum()
     local = True if Path("/core_srn").is_file() else False
     gw_conn = None
     if not local:
         # create and test gateway connection
         print("Initializing gateway connection...")
-        gw_conn = Connection(host='colosseum-gw', user=COLOSSEUM_USER)
+        gw_conn = Connection(host='colosseum-gw', user=colosseum.colosseum_user)
         if gw_conn.run('uname', hide=True, warn=False).failed:
             raise Exception('Gateway connection failed')
 
     # parse srn from json, add gateway and test
-    srn_list = parse_snr_from_reservation(args.reservation_id, args.srn_blacklist)
+    srn_list = colosseum.parse_snr_from_reservation(args.reservation_id, args.srn_blacklist)
     print('Testing srn connections...')
     for s_i, srn in enumerate(srn_list):
         srn.conn_gw = gw_conn
@@ -109,7 +55,7 @@ def manager_init(args, sounding):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='IAB-Manager', default_config_files=['./config.yaml'])
-    parser.add_argument('-r', '--reservation_id', type=str, required=True, help="Reservation id on Colosseum")
+    parser.add_argument('-r', '--reservation_id', type=str, required=False, help="Reservation id on Colosseum")
     parser.add_argument('-t', '--topology', type=str, help="Graphml file describing the topology of the IAB node")
     parser.add_argument('-s', '--scenario', type=str, help="RF scenario id")
     parser.add_argument('-sn', '--scenario_nodes', type=str, help="RF scenario number of nodes")
